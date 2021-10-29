e937b98660dd7112fdeff2a1a56123fd2ed361c1
ReNeeter <69535871+reneeter123@users.noreply.github.com>
2021-10-30 06:23:28 +0900

    変更

--- src/setup.py
+++ src/setup.py
@@ -3,7 +3,7 @@ import cx_Freeze
 cx_Freeze.setup(
     name="NanJ Script Regex Helper",
     version="1.0",
-    description="スクリプトのNG正規表現の制作を支援するソフト。",
+    description="なんJのスクリプトのNG正規表現の制作を支援するプログラム。",
     options={"build_exe": {"optimize": 2}},
     executables=[
         cx_Freeze.Executable("get_script_threadkeys.py"),
